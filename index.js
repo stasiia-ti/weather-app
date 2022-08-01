a0eab75f30ebd265d5b0c4be8e2c466478ba3994
Stasia Titovska <patrapema@gmail.com>
2022-08-01 13:11:22 +0300

    Forecast setting

--- index.js
+++ index.js
@@ -47,6 +47,44 @@ function formatDate(date) {
 let nowDate = document.querySelector("p.today");
 nowDate.innerHTML = formatDate(currentTime);
 
+//  Forecast of day
+
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = "";
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+              <div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                <div class="weather-forecast-temp">
+                  <span class="weather-forecast-max">27° </span>
+                  / <span class="weather-forecast-min">17°</span>
+                  <img
+                    src="http://openweathermap.org/img/wn/50d@2x.png"
+                    alt=""
+                    width="50"
+                  />
+                </div>
+              </div>`;
+  });
+
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
+// Coordinats of search city
+
+function getForecast(coordinates) {
+  let apiKey = "98f63ab964a6f17c8f87a4018e522a54";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 //   Weather of city search
 
 function temperature(response) {
@@ -60,6 +98,9 @@ function temperature(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = Math.round(response.data.main.humidity);
+  let pressureElement = document.querySelector("#pressure");
+  pressureElement.innerHTML = response.data.main.pressure;
+
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src",
@@ -69,6 +110,8 @@ function temperature(response) {
   citySearch.innerHTML = response.data.name;
 
   celsiusTemperature = response.data.main.temp;
+
+  getForecast(response.data.coord);
 }
 
 // Weather of geoloc
